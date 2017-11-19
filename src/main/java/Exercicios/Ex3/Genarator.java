c54cb1474bc8331f550b9f5644fef88ee42339b0
Pedro Almeida <41471@alunos.isel.ipl.pt>
2017-11-19 22:30:00 +0000

    update worktree

---
+++ src/main/java/Exercicios/Ex3/Genarator.java
@@ -0,0 +1,27 @@
+package Exercicios.Ex3;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public class Genarator {
+
+    public static void main(String[] args){
+
+        if(args.length < 1 ){
+            System.out.println("add file to arguments");
+            return;
+        }
+
+        File f = new File(args[0]);
+
+        try {
+            String str = Encode.getGetStringByPercentageMap(f, Charset.defaultCharset(), 100);
+            System.out.println(str);
+        } catch (IOException e) {
+            System.out.println("cant find or read the file");
+        }
+
+    }
+
+}
