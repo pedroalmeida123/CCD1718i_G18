07ed1d826a11ba4ffd6e161d0899a434f9417975
Rubben <40688@alunos.isel.ipl.pt>
2017-11-20 19:53:52 +0000

    Ex 5

---
+++ src/main/java/Exercicios/Ex5/Domain.java
@@ -0,0 +1,12 @@
+package Exercicios.Ex5;
+
+/**
+ * Created by Rúben Miranda on 20/11/2017.
+ */
+public class Domain {
+    Double rangeMin;
+    Double rangeMax;
+    String cod;
+    Double prob;
+    public Byte inByt;
+}
