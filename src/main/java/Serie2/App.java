4524fa22f3a78ba7003ed080280221c9fa1157ef
Rubben <40688@alunos.isel.ipl.pt>
2018-02-02 15:16:16 +0000

    correcao

--- src/main/java/Serie2/App.java
+++ src/main/java/Serie2/App.java
@@ -5,6 +5,11 @@ import com.jcraft.jzlib.Deflater;
 import com.jcraft.jzlib.GZIPException;
 import com.jcraft.jzlib.JZlib;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Arrays;
+
 /**
  * Created by Pedro on 12/12/2017.
  */
@@ -12,7 +17,7 @@ public class App extends IOUtils{
 
     public static long[][] matrixCompression;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String[] files = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
         matrixCompression = new long[files.length][files.length];
@@ -22,48 +27,64 @@ public class App extends IOUtils{
 
     }
 
-    public static void processDeflate(String[] arrayoffiles){
+    public static void processDeflate(String[] arrayoffiles) throws IOException {
 
-        String dicionary;
         for (int i = 0; i < arrayoffiles.length; i++) {
+            File file = new File("_sent_mail\\"+arrayoffiles[i]);
+            RandomAccessFile raf = new RandomAccessFile(file, "r");
+            int start=0;
+            int thismany = (int)file.length();
 
-            dicionary = readLineByLineJava8("_sent_mail\\"+arrayoffiles[i]);
+            if(file.length()> 32000){
+                start=(int)file.length()-32000;
+                thismany=32000;
+            }
+            byte[] dict = new byte[thismany];
+
+            raf.seek(start);
+            raf.read(dict, start, 2);
+
 
             for (int j = 0; j<arrayoffiles.length;j++){
-
-                String file = readLineByLineJava8("_sent_mail\\"+arrayoffiles[j]);
-                Deflater deflate=null;
-                int comprLen = 40000;
-                byte[] compressed = new byte[comprLen];
-                byte[] bytes = file.getBytes();
-                int err;
-
+                String compr= Arrays.toString(dict);
+                String line="";
+                Deflater deflate;
                 try {
                     deflate = new Deflater(JZlib.Z_BEST_COMPRESSION);
-                    deflate.setDictionary(dicionary.getBytes(),dicionary.getBytes().length);
-                    deflate.setInput(bytes);
-                    deflate.setOutput(compressed);
+                    while((line = readLineByLineJava8("_sent_mail\\"+arrayoffiles[j]))!=null){
+                        String val = compr.concat(line);
 
-                    while(true) {
-                        err = deflate.deflate(JZlib.Z_FINISH);
-                        if (err == JZlib.Z_STREAM_END) break;
+                        byte[] buffer = val.getBytes();
+                        byte[] compressed = new byte[buffer.length];
+
+                        int err;
+
+                        deflate.setInput(buffer);
+                        deflate.setOutput(compressed);
+
+                        while(true) {
+                            err = deflate.deflate(JZlib.Z_FINISH);
+                            if (err == JZlib.Z_STREAM_END) break;
+                        }
+                        compr="";
                     }
                     System.out.println("file" + i + " " + j);
-                    double out = deflate.getTotalOut();
-                    matrixCompression[i][j] = (deflate.getTotalIn()-out);
-
+                    double out = deflate.getTotalOut()-dict.length;
+                    matrixCompression[i][j] = (long) (deflate.getTotalIn()-out);
 
                 } catch (GZIPException e) {
                     e.printStackTrace();
                 }
+
             }
         }
         int val=0;
         int valprev=0;
         int dicID=0;
+        int i,f;
         for(i=0;i<matrixCompression.length;i++){
             for(f=0;f<matrixCompression.length;f++){
-                val+=matrixCompression[i][j];
+                val+=matrixCompression[i][f];
             }
             if(val>valprev){
                 dicID=i;
@@ -72,7 +93,7 @@ public class App extends IOUtils{
             System.out.println("ID->"+arrayoffiles[i]+" reduction->"+val);
         }
         System.out.println();
-        System.out.println("Best dictionary->"arrayoffiles[dicID]);
+        System.out.println("Best dictionary->"+arrayoffiles[dicID]);
     }
 
 }
